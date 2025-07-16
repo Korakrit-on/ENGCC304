fe3368cfb959272aa47a999365a22565d21e67dd
Korakrit Borisuth <captainza.055@gmail.com>
2025-07-17 01:09:04 +0700

    lab3 modify2

--- Lab/Lab3/Lab03-Codex.c
+++ Lab/Lab3/Lab03-Codex.c
@@ -2,35 +2,43 @@
 #include <math.h>
 
 int isPrime(int num) { 
-    if(num<2)
-    return 0;
+    if (num < 2)
+        return 0;
     for (int i = 2; i <= sqrt(num); i++) {
     if  (num % i == 0)
-    return 0;
+        return 0;
     }
-    return 1;
+        return 1;
 }
+
   int main() {
         int n;
+
         printf("Enter N : ");
-        scanf("%d",&n);
-        int arr[n]; 
-        for(int i = 0;i < n; i++) {
-        printf("Enter value[%d] : ", i);
-        scanf("%d", &arr[i]);
+        scanf("%d", &n);
+
+        int arr[n];
+
+        for(int i = 0; i < n; i++) {
+            printf("Enter value[%d] : ", i);
+            scanf("%d", &arr[i]);
         }
+
         printf("Index:  ");
         for (int i = 0; i < n; i++) {
-        printf("%2d ", i);
+            printf("%2d", i);
         }
+
         printf("\n");
         printf("Array:  ");
-        for(int i=0;i<n;i++) {
+        for(int i = 0; i < n; i++) {
             if (isPrime(arr[i]))
-        printf("%2d ", arr[i]); 
+                printf("%2d", arr[i]); 
             else
-        printf("%2s ", "#"); 
+                printf("%2s", "#"); 
         }
+
         printf("\n");
-        return 0;
+            return 0;
+
     }
