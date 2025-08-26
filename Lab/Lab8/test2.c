66b044be5a4b7e5e7db853d2fb091d2127e25bcf
Captain <captain@MacBook-Air-khxng-LOSTXKER.local>
2025-08-26 22:26:08 +0700

    add test2 lab2 flowchart

--- Lab/Lab8/test2.c
+++ Lab/Lab8/test2.c
@@ -1,65 +1,72 @@
 #include <stdio.h>
 int main() {
-    int choice , money, totalCost=0, price = 0;
+    int menu , money, totalCost=0, cost = 0;
     char addMore;
+
+
     printf( "===Vending Machine===\n" ) ;
     printf( "0. Coke 10 baht\n" ) ;
-    printf( "1. Green soda 12 baht\n" ) ;
+    printf( "1. Green juice 12 baht\n" ) ;
     printf( "2. Americano 30 baht\n" ) ;
     printf( "3. Latae 35 baht\n" ) ;
     printf( "4. Sunflower seed 13 baht\n" ) ;
     printf( "5. Bean 20 baht\n" ) ;
-    printf( "6. Pork 15 baht\n" ) ;
-    scanf( "%d", &choice ) ;//list of drinks
+    printf( "6. sour Pork 15 baht\n" ) ;
+    scanf( "%d", &menu ) ;//list of goods
 
-    switch( choice ) {
+    do {
+    switch( menu ) {
         case 0 :
-            price = 10 ; 
-            printf( "Coke %d baht\n",price ) ; 
+            cost = 10 ; 
+            printf( "Coke %d baht\n",cost ) ; 
             break ;
         case 1 : 
-            price = 12 ; 
-            printf( "Green soda %d baht\n",price ) ; 
+            cost = 12 ; 
+            printf( "Green soda %d baht\n",cost ) ; 
             break ;
         case 2 :
-            price = 30 ; 
-            printf( "Americano %d baht\n",price ) ; 
+            cost = 30 ; 
+            printf( "Americano %d baht\n",cost ) ; 
             break ; 
         case 3 :
-        price = 35 ; 
-            printf( "Latae %d baht\n",price ) ; 
+            cost = 35 ; 
+            printf( "Latae %d baht\n",cost ) ; 
             break ;    
         case 4 :
-            price = 13 ; 
-            printf( "Sunflower seed %d baht\n",price ) ; 
+            cost = 13 ; 
+            printf( "Sunflower seed %d baht\n",cost ) ; 
             break ;    
         case 5 :
-            price = 20 ; 
-            printf( "Bean %d baht\n",price ) ; 
+            cost = 20 ; 
+            printf( "Bean %d baht\n",cost ) ; 
             break ;
         case 6 :
-            price = 15 ; 
-            printf( "Pork %d baht\n",price ) ; 
+            cost = 15 ; 
+            printf( "sour pork %d baht\n",cost ) ; 
             break ;
         default :
-            printf( "===============================\n" "We Don't Have Your That Drinks\n" ) ;
+            printf(  "Plese choose menu in the list only(0-6) \n" ) ;
             break ;
             return 1 ;
     }//end switch
 
+    totalCost += cost;
     printf("total cost:%d\n ",totalCost);
-    
-    totalCost += price;
-
-
-
+    printf("do you want anything else?(y,n)\n");
+    scanf(" %c",&addMore);
+    if (addMore == 'y'){
+    printf("please choose your goods(0-6)\n");
+    scanf(" %d", &menu);
+    }
+}while (addMore == 'y');
+   
 
 
     printf( "Please put your money in : " );
     scanf( "%d", &money ) ;
-        if( money >= price ) {
-            int change = money - price ;
-            printf( "Please Receive Your Drinks\n" ) ;
+        if( money >= totalCost ) {
+            int change = money - totalCost ;
+            printf( "Please Receive Your Goods\n" ) ;
                 if (change > 0 ) {
                 printf ("Your change is : %d baht\n", change ) ;
                 }
@@ -67,6 +74,6 @@ int main() {
         } else {
             printf( "Don't have enough money . Please put in money\n" );
         }
-    return 0 ;//end เงินทอน
+    return 0 ;//end change
 }
-
+    
